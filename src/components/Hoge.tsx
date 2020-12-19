690c5cba20871b560af31f1ff8884e5b35f49b45
YusukeKumamoto <y.kuma1229@gmail.com>
2020-12-19 11:24:41 +0900

    feat(next): next.js追加と諸々設定

---
+++ src/components/Hoge.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Hoge = ()=> {
+  return (
+    <>Hogeです</>
+  )
+}
+
+export default Hoge
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next"
+import Hoge from "@/components/Hoge"
+
+const Index:NextPage = ()=> {
+  return(
+    <div>
+      Indexでーす
+      <Hoge />
+    </div>
+  ) 
+}
+
+export default Index
