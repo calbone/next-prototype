56d72942301945495402a276d316e95bc579e92f
YusukeKumamoto <y.kuma1229@gmail.com>
2021-01-04 19:04:12 +0900

    refactor(pages): カスタムアップ修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,53 @@
 import React from 'react';
+import { /* App,*/ AppProps /*AppContext*/ } from 'next/app';
 import Head from 'next/head';
-import { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '@/redux/createStore';
 
-export default function MyApp(props: AppProps) {
-  const { Component, pageProps } = props;
-
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <React.Fragment>
+    <Provider store={store}>
       <Head>
         <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </React.Fragment>
+      <Component {...pageProps} />
+    </Provider>
   );
-}
+};
+
+// MyApp.getInitialProps = async ({ Component, ctx }: AppContext) => {
+//   let pageProps = {};
+//   if (Component.getInitialProps) {
+//     pageProps = await Component.getInitialProps(ctx);
+//   }
+//   return { ...pageProps };
+// };
+
+export default MyApp;
+
+/**
+ * see:https://nextjs.org/docs/basic-features/typescript#custom-app
+ */
+
+// export default class MyApp extends App {
+//   static async getInitialProps({ Component, ctx }: AppContext) {
+//     let pageProps = {};
+//     if (Component.getInitialProps) {
+//       pageProps = await Component.getInitialProps(ctx);
+//     }
+//     return { pageProps };
+//   }
+//   render() {
+//     const { Component, pageProps } = this.props;
+//     return (
+//       <Provider store={store}>
+//         <Head>
+//           <title>My page</title>
+//           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+//         </Head>
+//         <Component {...pageProps} />
+//       </Provider>
+//     );
+//   }
+// }
